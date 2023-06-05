e8d17158784df269280e63865c3289b52201f8f4
decitizen <ville.saarinen@tuta.io>
2023-06-06 02:38:45 +0300

    Remove heuristics and move to OVNS repo

--- kmmi/heuristics/__init__.py
+++
@@ -1,7 +0,0 @@
-from kmmi.heuristics.initialize import *
-from kmmi.heuristics.neighborhood_search import *
-from kmmi.heuristics.neighborhood_change import *
-from kmmi.heuristics.utils import *
-from kmmi.heuristics.bvns import *
-from kmmi.heuristics.ovns import *
-from kmmi.heuristics.ovns_fs import *
