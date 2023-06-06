6c63b94db9e6e9bbca65827985c575936b7f4b98
decitizen <ville.saarinen@tuta.io>
2023-06-06 21:24:28 +0300

    Update enumeration to use igraph

--- kmmi/enumeration/graphlet_enumeration.py
+++ kmmi/enumeration/graphlet_enumeration.py
@@ -1,28 +1,26 @@
-import pymnet as pn
+import igraph as ig
+import networkx as nx
 
-def generate_candidate_subgraphs_esu(G, Vs, k_min, k_max, custom_processing_function=None):
-    u"""Find all graphlets (connected induced subgraphs) with number of nodes k_min <= n <= k_max.
-    NB! Treats the network as an undirected one, i.e. for directed graphs we use the 'weakly connected' notion of connectedness.
-    if custom_processing_function is None:
-        Returns: list, where each element is a list of nodes that form a graphlet
-    else:
-        calls custom_processing_function whenever a subgraph is found, with parameter ([nodelist],[layerlist])
-        (layerlist not relevant here, nodelist contains the subgraph nodes)
-        example: custom_processing_function = lambda sub: print(sub[0])
+def generate_candidate_subgraphs_esu(G, Vs, k_min, k_max, w_thres=0.0):
+    """Find all graphlets (connected induced subgraphs) with number of nodes 
+    k_min <= n <= k_max. NB! Treats the network as an undirected one, i.e. 
+    for directed graphs we use the 'weakly connected' notion of connectedness.
     """
-    magenta_G = G.subgraph(Vs)
-    M = pn.MultilayerNetwork(aspects=1,directed=False,fullyInterconnected=True)
-    M.add_layer(0)
-    for ii in magenta_G.nodes:
-        M.add_node(ii)
-    for u,v in magenta_G.edges:
-        M[u,v,0] = 1
-    if not custom_processing_function:
-        reslist = []
-        res = lambda sub : reslist.append(sub[0])
-    else:
-        res = custom_processing_function
+    Gt = nx.Graph()
+    for u,v,w in G.edges(data='weight'):
+        if (v,u) in Gt.edges: continue
+        if w >= w_thres and u in Vs and v in Vs:
+            Gt.add_edge(u,v)
+    
+    n = len(Gt)
+    node_map = dict(zip(Gt.nodes, range(n)))
+    rnode_map = dict(zip(range(n), Gt.nodes))
+    Gs = nx.relabel_nodes(Gt, node_map)
+    G_ig = ig.Graph.from_networkx(Gs)
+    
+    all_motifs = []
     for k in range(k_min,k_max+1):
-        pn.sampling.esu.sample_multilayer_subgraphs_esu(M,res,nnodes=k,nlayers=1)
-    if not custom_processing_function:
-        return reslist
+        motifs = []
+        G_ig.motifs_randesu(size=k, callback=lambda g, ids, iclass: motifs.append(ids))
+        all_motifs += motifs
+    return [[rnode_map[u] for u in motif] for motif in all_motifs]
